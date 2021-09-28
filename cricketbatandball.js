275742f43edfdc6fcf1564b489f3251cc4d7ae42
Rithvik39 <72606950+Rithvik39@users.noreply.github.com>
2021-09-28 16:58:47 +0530

    Add files via upload

---
+++ cricketbatandball.js
@@ -0,0 +1,49 @@
+img = "";
+Status = "";
+objects = [];
+function preload(){
+    img = loadImage("cricketbatandball.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(800 , 650);
+    canvas.center();
+    object_detector = ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function draw(){
+    image(img , 0 , 0 , 800 , 650);
+
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#ff2a00");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#ff2a00");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
+
+}
+
+function modelLoaded(){
+    console.log("model is loaded !!!");
+    Status = true;
+    object_detector.detect(img , gotResults);
+}
+
+function gotResults(error , results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function back(){
+    window.location = "index.html";
+}
